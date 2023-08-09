e37f4501434e396acfee9b2094151a9d38f5078d
Deepesh Gupta <guptad28031998@gmail.com>
2023-08-10 00:16:09 +0530

    Added succesfullt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String agrs[]) {
+
+        A objA = new A();
+        B objB = new B();
+
+        System.out.println(objA.meth());
+
+
+        System.out.println(objB.meth());
+    }
 }
